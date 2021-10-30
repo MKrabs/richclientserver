4f4248871703d356afbf7263c478f0f2278ad433
Iven Krall <iven.krall@bewotec.de>
2021-10-30 23:35:54 +0200

    Removed unused fields from and updated api of Todo

--- src/main/java/com/example/rich_client_server/Todo.java
+++ src/main/java/com/example/rich_client_server/Todo.java
@@ -12,13 +12,10 @@ public class Todo {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title;
-    private String description;
     private boolean done;
 
-    public Todo(Long id, String title, String description, boolean done) {
-        this.id = id;
+    public Todo(String title, boolean done) {
         this.title = title;
-        this.description = description;
         this.done = done;
     }
 
@@ -40,14 +37,6 @@ public class Todo {
 
     public void setTitle(String title) {
         this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public boolean isDone() {
