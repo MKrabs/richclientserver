63f3925d5dde0e9843a93019288e655ed325665c
Iven Krall <iven.krall@bewotec.de>
2021-10-30 23:28:32 +0200

    Removed solution

--- src/main/java/com/example/rich_client_server/TodoController.java
+++ src/main/java/com/example/rich_client_server/TodoController.java
@@ -1,37 +1,7 @@
 package com.example.rich_client_server;
 
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(value = "/todo")
+/**
+ * Nimmt die Anfragen vom Client entgegen und leitet diese ans Repository weiter.
+ */
 public class TodoController {
-
-    private final TodoRepository todoRepository;
-
-    @Autowired
-    public TodoController(TodoRepository todoRepository) {
-        this.todoRepository = todoRepository;
-    }
-
-    @GetMapping
-    public List<Todo> getAllTodos() {
-        return todoRepository.findAll();
-    }
-
-    @GetMapping("/{id}")
-    public Todo findById(@PathVariable Long id) {
-        return todoRepository.find(id);
-    }
-
-    @PostMapping
-    public Todo save(@RequestBody Todo todo) {
-        return todoRepository.save(todo);
-    }
 }
--- src/main/java/com/example/rich_client_server/TodoRepository.java
+++ src/main/java/com/example/rich_client_server/TodoRepository.java
@@ -1,27 +1,8 @@
 package com.example.rich_client_server;
 
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
+/**
+ * Wrapped das {@link SpringTodoRepository} um die standard Methoden zu verbergen
+ * und eine eigene Schnittstelle zu definieren.
+ */
 public class TodoRepository {
-
-    private final SpringTodoRepository repository;
-
-    public TodoRepository(@Autowired SpringTodoRepository repository) {
-        this.repository = repository;
-    }
-
-    public List<Todo> findAll() {
-        return repository.findAll();
-    }
-
-    public Todo find(Long id) {
-        return repository.findById(id).orElse(null);
-    }
-
-    public Todo save(Todo todo) {
-        return repository.save(todo);
-    }
 }
